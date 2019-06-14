064e25db60404623a7a2c0084f3b3c8ce9a49e69
Dominik Schmidt <dominikschmidt1893@gmail.com>
2019-06-13 22:49:04 -0700

    Finished login screen

--- app/src/main/java/com/example/distributednfcchatapp/SignInActivity.java
+++ app/src/main/java/com/example/distributednfcchatapp/SignInActivity.java
@@ -2,12 +2,19 @@ package com.example.distributednfcchatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.os.*;
 
 public class SignInActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
     }
 }
