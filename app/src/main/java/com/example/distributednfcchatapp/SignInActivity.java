026a6479d3621abde0a7f07494156b4174b58880
Dominik Schmidt <dominikschmidt1893@gmail.com>
2019-06-13 23:24:48 -0700

    Boiler Plate Login

--- app/src/main/java/com/example/distributednfcchatapp/SignInActivity.java
+++ app/src/main/java/com/example/distributednfcchatapp/SignInActivity.java
@@ -1,13 +1,20 @@
 package com.example.distributednfcchatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.os.*;
 
 public class SignInActivity extends AppCompatActivity {
-
+    private EditText userName;
+    private EditText password;
+    private Button loginButton;
+    private Button signUpButton;
+    private Button forgotPasswordButton;
 
 
 
@@ -16,5 +23,27 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        userName = (EditText) findViewById(R.id.userNameText);
+        password = (EditText) findViewById(R.id.passwordText);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+        forgotPasswordButton = (Button) findViewById(R.id.forgotPasswordButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(userName.getText().toString(), password.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String _userName, String _userPassword) {
+        if ((_userName.equals("User")) && (_userPassword.equals("Password"))) {
+            Intent intent = new Intent(SignInActivity.this, FrontPageActivity.class);
+            startActivity(intent);
+        } else {
+
+        }
     }
 }
